9c2aa084aebacb07c3dc1f6d1f837c2a1ad10e11
pragya-codes <pragya21offc@gmail.com>
2023-12-24 11:21:20 +0530

    Favicon, header styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,22 @@ function Navbar() {
 	return (
 		<>
 			<div className="navbar">
-				<div className="logo">
-					UniChic<span>.</span>
+				<div className="container">
+					<div className="logo">
+						UniChic<span>.</span>
+					</div>
+					<ul className="menu">
+						<li>Home</li>
+						<li>Shop</li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+					<ul className="side">
+						<li>Login/Register</li>
+						<li>Cart</li>
+						<li>Like</li>
+					</ul>
 				</div>
-				<ul className="menu">
-					<li>Home</li>
-					<li>Shop</li>
-					<li>About</li>
-					<li>Contact</li>
-				</ul>
-				<ul className="side">
-					<li>Login/Register</li>
-					<li>Cart</li>
-					<li>Like</li>
-				</ul>
 			</div>
 		</>
 	);
