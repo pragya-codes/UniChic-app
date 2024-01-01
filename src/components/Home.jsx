619f01784b0af87052162d4d57155a882b9148e2
pragya-codes <pragya21offc@gmail.com>
2024-01-01 13:34:24 +0530

    Swiper Navigation added

---
+++ src/components/Home.jsx
@@ -0,0 +1,54 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../styles/Home.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function Home() {
+	return (
+		<>
+			<div className="hero">
+				<div className="explore">
+					<p>Explore all &rarr;</p>
+					<img src="src\assets\explore.png" alt="explore page"></img>
+				</div>
+				<Swiper
+					spaceBetween={30}
+					centeredSlides={true}
+					autoplay={{
+						delay: 2500,
+						disableOnInteraction: false,
+					}}
+					pagination={{
+						clickable: true,
+					}}
+					navigation={true}
+					modules={[Autoplay, Pagination, Navigation]}
+					className="mySwiper"
+				>
+					<SwiperSlide className="swiper-slide">
+						<p>Men &rarr;</p>
+						<img src="src\assets\shirts.jpg"></img>
+					</SwiperSlide>
+
+					<SwiperSlide className="swiper-slide">
+						<p>Women &rarr;</p>
+						<img src="src\assets\women.jpg"></img>
+					</SwiperSlide>
+
+					<SwiperSlide className="swiper-slide">
+						<p>Jewllery &rarr;</p>
+						<img src="src\assets\jewellery.jpg"></img>
+					</SwiperSlide>
+				</Swiper>
+			</div>
+		</>
+	);
+}
